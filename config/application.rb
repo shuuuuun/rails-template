37157337e27f340a53c400ca60ea41ab06e4d799
motoki-shun <motoki-shun@kayac.com>
2017-03-01 18:13:42 +0900

    add some config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,22 @@ module RailsTemplate
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # time zone
+    config.time_zone = 'Tokyo'
+
+    # locale
+    config.i18n.default_locale = :ja
+    config.i18n.fallbacks = [:en]
+
+    # add autoload path
+    # config.autoload_paths += %W(#{config.root}/lib)
+
+    # change error field html output
+    # config.action_view.field_error_proc = proc { |html_tag, instance| "<span class='field_with_errors'>#{html_tag}</span>".html_safe }
+
+    # disable generate assets
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
   end
 end
